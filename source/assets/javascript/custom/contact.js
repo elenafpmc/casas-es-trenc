929309ef8b276c9efe69fd9b55acd35d006c9273
María Peña Jaquete <maria.pena.jaquete@gmail.com>
2018-05-21 13:59:02 +0200

    airport marker size

--- source/assets/javascript/custom/contact.js
+++ source/assets/javascript/custom/contact.js
@@ -17,10 +17,10 @@ if ( $('#map').length > 0 ) {
     };
     var imageAirport = {
       url: 'http://development.flyingpigs.es/casas-es-trenc/build/assets/images/content/airport-marker.png',
-      size: new google.maps.Size(150, 150),
+      size: new google.maps.Size(50, 74),
       origin: new google.maps.Point(0, 0),
-      anchor: new google.maps.Point(37, 37),
-      scaledSize: new google.maps.Size(75, 75)
+      anchor: new google.maps.Point(25, 37),
+      scaledSize: new google.maps.Size(50, 74)
     };
     var markerVentas = new google.maps.Marker({
       position: ventas,
