e3c8e19e1b537886b2fedafc62bce660b263b022
María Peña Jaquete <maria.pena.jaquete@gmail.com>
2018-06-04 13:19:49 +0200

    minor fix

--- resources/lang/es/sectores.php
+++ resources/lang/es/sectores.php
@@ -2,6 +2,6 @@
 return[
     'TITLE'                    => 'Pisos desde',
 	'DESDE_219'                => 'Desde 219.000€ a 249.000€',
-    'DESCRIPCION_AVE'          => 'En el sector Ave del Paraíso podrá disfrutar de una piscina para adultos de 20x11cm y otra piscina circular infantil de 8m de diámetro, zona solárium y una gran zona verde donde encontrará especies vegetales seleccionadas. Además, cuenta con plazas de aparcamiento privadas.',
+    'DESCRIPCION_AVE'          => 'En el sector Ave del Paraíso podrá disfrutar de una piscina para adultos de 20x11 metros y otra piscina circular infantil de 8 metros de diámetro, zona solárium y una gran zona verde donde encontrará especies vegetales seleccionadas. Además, cuenta con plazas de aparcamiento privadas.',
     'COCINA_AMERICANA'         => 'Cocina americana',
 ];
