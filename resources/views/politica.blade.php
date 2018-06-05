6405b40bfd93dc5a230c1a35404896de616e8b0f
fp-ultano <ultano@flyingpigs.es>
2018-06-05 13:04:16 +0200

    sectores and politica translations

--- resources/views/politica.blade.php
+++ resources/views/politica.blade.php
@@ -12,43 +12,43 @@
     <div class="row center-xs">
       <div class="col-sm-10 col-xs-12">
         <p class="static__title">
-          POLÍTICA DE PRIVACIDAD
+          {{ __('politica.POLITICA_DE_PRIVACIDAD') }}
         </p>
         <div class="static__text">
           <p>
-            A los efectos de lo previsto en la Ley Orgánica 15/1999, de 13 de diciembre, de Protección de Datos de Carácter Personal, se informa a los usuarios de esta página Web de que los datos de carácter personal que faciliten a través de ella, tanto cumplimentando cualquier formulario, como a través de correo electrónico o por cualquier otro medio serán tratados de conformidad con la presente Política de Protección de Datos.
+            {{ __('politica.A_LOS_EFECTOS_DE_LO') }}
           </p>
           <p>
-            Los datos facilitados pasarán a formar parte de los ficheros de los que es responsable URNOVA, S.L.U. y que se encuentran convenientemente inscritos en el Registro General de la Agencia Española de Protección de Datos. En función del fin para el que los datos sean facilitados, estos serán tratados con objeto de dar respuesta a su solicitud, consulta, sugerencia o duda.
+            {{ __('politica.LOS_DATOS_FACILITADOS') }}
           </p>
           <p>
-            Si se nos proporcionan datos de terceros, la persona que lo haga deberá, bajo su responsabilidad, informar al afectado de lo dispuesto en esta cláusula y obtener previamente su consentimiento.
+            {{ __('politica.SI_SE_NOS_PROPORCIONA') }}
           </p>
           <p>
-            En todo caso, pueden ejercerse los derechos de acceso, rectificación, cancelación y oposición en la siguiente dirección: C/ Joan Maragall, 38, 07002 (Palma de Mallorca) o mandando un e-mail a <a href="mailto:business@urnova.com">business@urnova.com</a>.
+            {{ __('politica.EN_TODO_CASO') }}
           </p>
         </div>
         <p class="static__title">
-          POLÍTICA DE COOKIES
+          {{ __('politica.POLITICA_DE_COOKIES') }}
         </p>
         <div class="static__text">
           <p>
-            Una cookie es un fichero que se descarga en su ordenador al acceder a determinadas páginas web. Las cookies permiten a una página web, entre otras cosas, almacenar y recuperar información sobre los hábitos de navegación de un usuario o de su equipo y, dependiendo de la información que contengan y de la forma en que se utilice su equipo, pueden servir para reconocerlo.
+            {{ __('politica.UNA_COOKIE_ES_UN_FICHERO') }}
           </p>
           <p>
-            No obstante, estos medios sólo obtienen información relacionada con el número de páginas visitas, la ciudad a la que está asignada la dirección IP desde la que se accede, el número de nuevos usuarios, la frecuencia y reincidencia de las visitas, el tiempo de visita, el navegador o el operador o tipo de terminal desde el que se realiza la visita. En ningún caso se obtienen datos sobre el nombre, apellidos o dirección postal desde la que el usuario se ha conectado.
+            {{ __('politica.NO_OBSTANTE_ESTOS_MEDIOS') }}
           </p>
           <p>
-            Las cookies utilizadas en esta página Web y la concreta finalidad de cada una de ellas son las siguientes:
+            {{ __('politica.LAS_COOKIES_UTILIZADAS') }}
           </p>
 
           <ul>
-            <li>Cookie _ga: Esta cookie tiene como finalidad controlar las visitas únicas. La primera vez que un usuario entre en el sitio Web de URNOVA, S.L.U. a través de un navegador se instalará esta cookie. Cuando este usuario vuelva a entrar en nuestra página con el mismo navegador, la cookie considerará que es el mismo usuario. Solo en el caso de que el usuario cambie de navegador, Google Analytics lo considerará otro usuario.</li>
-            <li>Cookie datr: Cookies de Facebook que tienen por finalidad posibilitar el logueo en el sitio web a través de la cuenta de Facebook.</li>
+            <li>{{ __('politica.COOKIE_GA') }}</li>
+            <li>{{ __('politica.COOKIE_DATR') }}</li>
           </ul>
 
           <p>
-            Puede usted permitir, bloquear o eliminar las cookies instaladas en su equipo mediante la configuración de las opciones de su navegador. Puede encontrar información sobre cómo hacerlo, en relación con los navegadores más comunes, en los links que se incluyen a continuación:
+            {{ __('politica.PUEDE_USTED_PERMITIR') }}
           </p>
 
 
@@ -60,7 +60,7 @@
           </ul>
 
           <p>
-            Le informamos, no obstante, de la posibilidad de que la desactivación de alguna cookie impida o dificulte la navegación o la prestación de los servicios ofrecidos en esta Web.
+            {{ __('politica.LE_INFORMAMOS_NO_OBSTANTE') }}
           </p>
         </div>
       </div>
--- resources/views/sectores.blade.php
+++ resources/views/sectores.blade.php
@@ -12,76 +12,76 @@
   <div class="sector__header" style="background-image: url(../assets/images/content/franciscogarvi.com_B7K7803.jpg);">
     <div class="sector__header__content">
       <p class="sector__header__content__title">Ave del Paraíso</p>
-        <div class="sector__header__content__text"><p class="text">Desde</p> <p class="price">220.000€</p> <p class="text">Últimos 9 pisos disponibles.</p></div>
+        <div class="sector__header__content__text"><p class="text">{{ __('sectores.DESDE') }}</p> <p class="price">220.000€</p> <p class="text">{{ __('sectores.ULTIMOS_9_PISOS') }}</p></div>
         <!-- <div class="cnt-button"> -->
         <!-- </div> -->
       <button type="button" class="btn-arrow-down btn-sector"><span class="icon-arrow-down"></span></button>
     </div>
   </div>
   <div class="sector__content">
-    <div class="sector__content__info"><img src="../assets/images/content/logo-avedelparaiso.png" class="sector__content__info__logo" alt="Logo avedelparaiso" /><p class="sector__content__info__title">Apartamentos Ave del Paraíso</p>
+    <div class="sector__content__info"><img src="../assets/images/content/logo-avedelparaiso.png" class="sector__content__info__logo" alt="Logo avedelparaiso" /><p class="sector__content__info__title">{{ __('sectores.APARTAMENTOS_AVE_DEL') }}</p>
                   <div class="sector__content__info__text">
-                    <p>En el sector Ave del Paraíso podrá disfrutar de una piscina para adultos de 20x11 metros y otra piscina circular infantil de 8 metros de diámetro, zona solárium y una gran zona verde donde encontrará especies vegetales seleccionadas. Además, cuenta con plazas de aparcamiento privadas.</p>
+                    <p>{{ __('sectores.DESCRIPCION_AVE') }}</p>
                     <p class="price">
-                      <span class="small">Pisos desde</span>
+                      <span class="small">{{ __('sectores.PISOS_DESDE') }}</span>
                        220.000€
                     </p>
                   </div>
                 </div>
       <div class="sector__content__list">
           <div class="sector__content__list__item">
-            <img src="../assets/images/content/cocina-americana-sectores-avedelparaiso.jpg" class="item-image" alt="Cocina americana sectores avedelparaiso" />
+            <img src="../assets/images/content/cocina-americana-sectores-avedelparaiso.jpg" class="item-image" alt="{{ __('sectores.COCINA_AMERICANA') }} {{ __('sectores.SECTORES') }} avedelparaiso" />
 
             <p class="title">
-              Cocina americana
+              {{ __('sectores.COCINA_AMERICANA') }}
             </p>
             <p class="text">
-              Todas las cocinas están equipadas y amuebladas con muebles de la marca alemana Nolte Kuechen y electrodomésticos de Balay.
+              {{ __('sectores.TODAS_LAS_COCINAS_ESTAN') }}
             </p>
           </div>
           <div class="sector__content__list__item">
             <img src="../assets/images/content/franciscogarvi.com_B7K7826.jpg" class="item-image" alt="Franciscogarvi.com b7k7826" />
             <p class="title">
-              Salón con vistas
+              {{ __('sectores.SALON_CON_VISTAS') }}
             </p>
             <p class="text">
-              Amplio salón comedor, desde el que podrá disfrutar de unas vistas privilegiadas en un entorno rodeado de naturaleza a escasos metros de la playa.
+              {{ __('sectores.AMPLIO_SALON_COMEDOR') }}
             </p>
           </div>
           <div class="sector__content__list__item">
-            <img src="../assets/images/content/franciscogarvi.com_B7K3487.jpg" class="item-image" alt="Banos completos sectores buganvilla" />
+            <img src="../assets/images/content/franciscogarvi.com_B7K3487.jpg" class="item-image" alt="{{ __('sectores.BANOS_COMPLETOS') }} {{ __('sectores.SECTORES') }} buganvilla" />
             <p class="title">
-              2 baños completos
+              {{ __('sectores.DOS_BANOS_COMPLETOS') }}
             </p>
             <p class="text">
-              Ambos baños constan de encimera de mármol crema marfil, sanitarios y espejo. En uno de los baños se encuentran el termo y la lavadora.
+              {{ __('sectores.AMBOS_BANOS_CONSTAN_DE') }}
             </p>
           </div>
           <div class="sector__content__list__item">
-            <img src="../assets/images/content/franciscogarvi.com_B7K3480.jpg" class="item-image" alt="Dormitorios dobles sectores buganvilla" />
+            <img src="../assets/images/content/franciscogarvi.com_B7K3480.jpg" class="item-image" alt="{{ __('sectores.DORMITORIOS_DOBLES') }} {{ __('sectores.SECTORES') }} buganvilla" />
             <p class="title">
-              2 dormitorios dobles
+              {{ __('sectores.DOS_DORMITORIOS_DOBLES') }}
             </p>
             <p class="text">
-              Los dormitorios están equipados y cuentan con armarios vestidos en su interior.
+              {{ __('sectores.LOS_DORMITORIOS_ESTAN') }}
             </p>
           </div>
           <div class="sector__content__list__item">
             <img src="../assets/images/content/franciscogarvi.com_B7K3507.jpg" class="item-image" alt="Franciscogarvi.com b7k3507" />
             <p class="title">
-              Amplias terrazas
+              {{ __('sectores.AMPLIAS_TERRAZAS') }}
             </p>
             <p class="text">
-              Los apartamentos están dotados de amplias terrazas que le permitirán disfrutar de unas bonitas vistas a los jardines y/o piscina.
+              {{ __('sectores.LOS_APARTAMENTOS_ESTAN') }}
             </p>
           </div>
           <div class="sector__content__list__item">
             <img src="../assets/images/content/franciscogarvi.com_B7K3439.jpg" class="item-image" alt="Franciscogarvi.com b7k3439" />
             <p class="title">
-              Zona común con jardines y piscinas
+              {{ __('sectores.ZONA_COMUN_CON_JARDINES') }}
             </p>
             <p class="text">
-              Consta de dos piscinas, una de adultos y otra infantil y una zona de solarium. Los jardines están formados por grandes plantas, árboles originales de la zona y delicadas flores.
+              {{ __('sectores.CONSTA_DE_DOS_PISCINAS') }}
             </p>
           </div>
       </div>
@@ -99,19 +99,19 @@
   <div class="sector__header" style="background-image: url(../assets/images/content/sector-buganvilla.jpg);">
     <div class="sector__header__content">
       <p class="sector__header__content__title">Buganvilla</p>
-        <div class="sector__header__content__text"><p class="text">Desde</p> <p class="price">295.000€</p></div>
+        <div class="sector__header__content__text"><p class="text">{{ __('sectores.CONSTA_DE_DOS_PISCINAS') }}</p> <p class="price">295.000€</p></div>
         <!-- <div class="cnt-button"> -->
         <!-- </div> -->
       <button type="button" class="btn-arrow-down btn-sector"><span class="icon-arrow-down"></span></button>
     </div>
   </div>
   <div class="sector__content">
-    <div class="sector__content__info"><img src="../assets/images/content/logo-buganvilla.png" class="sector__content__info__logo" alt="Logo buganvilla" /><p class="sector__content__info__title">Apartamentos Buganvilla</p>
+    <div class="sector__content__info"><img src="../assets/images/content/logo-buganvilla.png" class="sector__content__info__logo" alt="Logo buganvilla" /><p class="sector__content__info__title">{{ __('sectores.APARTAMENTOS_BUGANVILLA') }}</p>
                   <div class="sector__content__info__text">
-                    <p>El sector Buganvilla cuenta con 15.000 metros cuadrados de jardines, además de una gran piscina de agua salada de 33x14 metros más una piscina para niños y parque infantil, gimnasio con máquinas de última generación, sauna Freixenet, y una zona habilitada para la custodia y mantenimiento de bicicletas.</p>
+                    <p>{{ __('sectores.EL_SECTOR_BUGANVILLA') }}</p>
                     <p class="price">
-                      <span class="small">Pisos desde</span>
-                       295.000€ a 360.000€
+                      <span class="small">{{ __('sectores.PISOS_DESDE') }}</span>
+                       {{ __('sectores.295_A_360') }}
                     </p>
                   </div>
                 </div>
@@ -119,55 +119,55 @@
           <div class="sector__content__list__item">
             <img src="../assets/images/content/franciscogarvi.com_B7K3457.jpg" class="item-image" alt="Franciscogarvi.com b7k3457" />
             <p class="title">
-              Cocina americana
+              {{ __('sectores.COCINA_AMERICANA') }}
             </p>
             <p class="text">
-              Cocinas equipadas con electrodomésticos Balay y muebles en color wengué de la prestigiosa marca alemana Nolte Kuechen.
+              {{ __('sectores.COCINAS_EQUIPADAS_CON') }}
             </p>
           </div>
           <div class="sector__content__list__item">
             <img src="../assets/images/content/franciscogarvi.com_B7K3449.jpg" class="item-image" alt="Franciscogarvi.com b7k3449" />
             <p class="title">
-              Salón con vistas
+              {{ __('sectores.SALON_CON_VISTAS') }}
             </p>
             <p class="text">
-              Disfrute del salón comedor amueblado dotado de vistas a los jardines de la urbanización.
+              {{ __('sectores.DISFRUTE_DEL_SALON') }}
             </p>
           </div>
           <div class="sector__content__list__item">
             <img src="../assets/images/content/banos-completos-sectores-buganvilla.jpg" class="item-image" alt="Franciscogarvi.com b7k3487" />
             <p class="title">
-              2 baños completos
+              {{ __('sectores.DOS_BANOS_COMPLETOS') }}
             </p>
             <p class="text">
-              Cada apartamento consta de dos baños con encimera de mármol en color crema marifl, sanitarios de marca Roca y grifería de Grohe. Materiales de primera calidad que se aprecian en cada rincón.
+              {{ __('sectores.CADA_APARTAMENTO_CONSTA') }}
             </p>
           </div>
           <div class="sector__content__list__item">
             <img src="../assets/images/content/franciscogarvi.com_B7K4299.jpg" class="item-image" alt="Franciscogarvi.com b7k3480" />
             <p class="title">
-              Zona común con jardines y piscinas
+              {{ __('sectores.ZONA_COMUN_CON_JARDINES') }}
             </p>
             <p class="text">
-              Dos amplias piscinas, una de adultos y una infantil, con sistema de filtrado de arena y sal y zona de solarium se encuentran dentro de un jardín con 169 diferentes tipos de plantas, en una superficie de 15.000 metros cuadrados.
+              {{ __('sectores.DOS_AMPLIAS_PISCINAS') }}
             </p>
           </div>
           <div class="sector__content__list__item">
             <img src="../assets/images/content/bicicletas-buganvilla.jpg" class="item-image" alt="Franciscogarvi.com b7k3507" />
             <p class="title">
-              Instalaciones para bicicletas
+              {{ __('sectores.INSTALACIONES_PARA_BICIS') }}
             </p>
             <p class="text">
-              La urbanización cuenta con una zona de mantenimiento para sus bicicletas donde podrá ponerlas a punto con diversas herramientas para sus paseos por la isla.
+              {{ __('sectores.LA_URBANIZACION_CUENTA') }}
             </p>
           </div>
           <div class="sector__content__list__item">
             <img src="../assets/images/content/gimnasio-buganvilla.jpg" class="item-image" alt="Franciscogarvi.com b7k4299" />
             <p class="title">
-              Gimnasio
+              {{ __('sectores.GIMNASIO') }}
             </p>
             <p class="text">
-              Zona independiente para actividades de ocio con un área especialmente dedicada a gimnasio exclusivo para residentes.
+              {{ __('sectores.ZONA_INDEPENDIENTE_PARA') }}
             </p>
           </div>
       </div>
