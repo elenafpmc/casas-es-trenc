519bde3b7f70515fdae4a93db71e722578a5a7a7
María Peña Jaquete <maria.pena.jaquete@gmail.com>
2018-06-05 11:14:55 +0200

    sectores texts and images

--- resources/views/sectores.blade.php
+++ resources/views/sectores.blade.php
@@ -36,7 +36,7 @@
               Cocina americana
             </p>
             <p class="text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eleifend et enim vitae consectetur. Quisque et aliquet elit.
+              Todas las cocinas están equipadas y amuebladas con muebles de la marca alemana Nolte Kuechen y electrodomésticos de Balay.
             </p>
           </div>
           <div class="sector__content__list__item">
@@ -45,25 +45,25 @@
               Salón con vistas
             </p>
             <p class="text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eleifend et.
+              Amplio salón comedor, desde el que podrá disfrutar de unas vistas privilegiadas en un entorno rodeado de naturaleza a escasos metros de la playa.
             </p>
           </div>
           <div class="sector__content__list__item">
-            <img src="../assets/images/content/banos-completos-sectores-buganvilla.jpg" class="item-image" alt="Banos completos sectores buganvilla" />
+            <img src="../assets/images/content/franciscogarvi.com_B7K3487.jpg" class="item-image" alt="Banos completos sectores buganvilla" />
             <p class="title">
               2 baños completos
             </p>
             <p class="text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eleifend et enim vitae consectetur. Quisque et aliquet elit.
+              Ambos baños constan de encimera de mármol crema marfil, sanitarios y espejo. En uno de los baños se encuentran el termo y la lavadora.
             </p>
           </div>
           <div class="sector__content__list__item">
-            <img src="../assets/images/content/dormitorios-dobles-sectores-buganvilla.jpg" class="item-image" alt="Dormitorios dobles sectores buganvilla" />
+            <img src="../assets/images/content/franciscogarvi.com_B7K3480.jpg" class="item-image" alt="Dormitorios dobles sectores buganvilla" />
             <p class="title">
               2 dormitorios dobles
             </p>
             <p class="text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eleifend et.
+              Los dormitorios están equipados y cuentan con armarios vestidos en su interior.
             </p>
           </div>
           <div class="sector__content__list__item">
@@ -72,7 +72,7 @@
               Amplias terrazas
             </p>
             <p class="text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eleifend et enim vitae consectetur. Quisque et aliquet elit.
+              Los apartamentos están dotados de amplias terrazas que le permitirán disfrutar de unas bonitas vistas a los jardines y/o piscina.
             </p>
           </div>
           <div class="sector__content__list__item">
@@ -81,7 +81,7 @@
               Zona común con jardines y piscinas
             </p>
             <p class="text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eleifend et.
+              Consta de dos piscinas, una de adultos y otra infantil y una zona de solarium. Los jardines están formados por grandes plantas, árboles originales de la zona y delicadas flores.
             </p>
           </div>
       </div>
@@ -108,7 +108,7 @@
   <div class="sector__content">
     <div class="sector__content__info"><img src="../assets/images/content/logo-buganvilla.png" class="sector__content__info__logo" alt="Logo buganvilla" /><p class="sector__content__info__title">Apartamentos Buganvilla</p>
                   <div class="sector__content__info__text">
-                    <p>El sector Buganvilla cuenta con 15.000m2 de jardines, además de una gran piscina de agua salada de 33x14 metros más una piscina para niños y parque infantil, gimnasio con máquinas de última generación, sauna Freixenet, y una zona habilitada para la custodia y mantenimiento de bicicletas.</p>
+                    <p>El sector Buganvilla cuenta con 15.000 metros cuadrados de jardines, además de una gran piscina de agua salada de 33x14 metros más una piscina para niños y parque infantil, gimnasio con máquinas de última generación, sauna Freixenet, y una zona habilitada para la custodia y mantenimiento de bicicletas.</p>
                     <p class="price">
                       <span class="small">Pisos desde</span>
                        295.000€ a 360.000€
@@ -122,7 +122,7 @@
               Cocina americana
             </p>
             <p class="text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eleifend et enim vitae consectetur. Quisque et aliquet elit.
+              Cocinas equipadas con electrodomésticos Balay y muebles en color wengué de la prestigiosa marca alemana Nolte Kuechen.
             </p>
           </div>
           <div class="sector__content__list__item">
@@ -131,43 +131,43 @@
               Salón con vistas
             </p>
             <p class="text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eleifend et.
+              Disfrute del salón comedor amueblado dotado de vistas a los jardines de la urbanización.
             </p>
           </div>
           <div class="sector__content__list__item">
-            <img src="../assets/images/content/franciscogarvi.com_B7K3487.jpg" class="item-image" alt="Franciscogarvi.com b7k3487" />
+            <img src="../assets/images/content/banos-completos-sectores-buganvilla.jpg" class="item-image" alt="Franciscogarvi.com b7k3487" />
             <p class="title">
               2 baños completos
             </p>
             <p class="text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eleifend et enim vitae consectetur. Quisque et aliquet elit.
+              Cada apartamento consta de dos baños con encimera de mármol en color crema marifl, sanitarios de marca Roca y grifería de Grohe. Materiales de primera calidad que se aprecian en cada rincón.
             </p>
           </div>
           <div class="sector__content__list__item">
-            <img src="../assets/images/content/franciscogarvi.com_B7K3480.jpg" class="item-image" alt="Franciscogarvi.com b7k3480" />
-            <p class="title">
-              2 dormitorios dobles
-            </p>
-            <p class="text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eleifend et.
-            </p>
-          </div>
-          <div class="sector__content__list__item">
-            <img src="../assets/images/content/franciscogarvi.com_B7K3507.jpg" class="item-image" alt="Franciscogarvi.com b7k3507" />
-            <p class="title">
-              Amplias terrazas
-            </p>
-            <p class="text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eleifend et enim vitae consectetur. Quisque et aliquet elit.
-            </p>
-          </div>
-          <div class="sector__content__list__item">
-            <img src="../assets/images/content/franciscogarvi.com_B7K4299.jpg" class="item-image" alt="Franciscogarvi.com b7k4299" />
+            <img src="../assets/images/content/franciscogarvi.com_B7K4299.jpg" class="item-image" alt="Franciscogarvi.com b7k3480" />
             <p class="title">
               Zona común con jardines y piscinas
             </p>
             <p class="text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eleifend et.
+              Dos amplias piscinas, una de adultos y una infantil, con sistema de filtrado de arena y sal y zona de solarium se encuentran dentro de un jardín con 169 diferentes tipos de plantas, en una superficie de 15.000 metros cuadrados.
+            </p>
+          </div>
+          <div class="sector__content__list__item">
+            <img src="../assets/images/content/bicicletas-buganvilla.jpg" class="item-image" alt="Franciscogarvi.com b7k3507" />
+            <p class="title">
+              Instalaciones para bicicletas
+            </p>
+            <p class="text">
+              La urbanización cuenta con una zona de mantenimiento para sus bicicletas donde podrá ponerlas a punto con diversas herramientas para sus paseos por la isla.
+            </p>
+          </div>
+          <div class="sector__content__list__item">
+            <img src="../assets/images/content/gimnasio-buganvilla.jpg" class="item-image" alt="Franciscogarvi.com b7k4299" />
+            <p class="title">
+              Gimnasio
+            </p>
+            <p class="text">
+              Zona independiente para actividades de ocio con un área especialmente dedicada a gimnasio exclusivo para residentes.
             </p>
           </div>
       </div>
